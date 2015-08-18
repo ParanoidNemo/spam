6cead30b0ab9040bfaaebbde4d738efa6b2dab52
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-08-18 19:10:37 +0200

    create archive module - add compress function (initial commit)

---
+++ archive.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2015 by Andrea Calzavacca <paranoid.nemo@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#import std module(s)
+import os, sys
+import shutil
+
+#import custom module(s)
+import check
+
+def compress(self='new_archive', extension='.tar', location, path=os.path.expanduser('~/.local/share/be.shell/backup/')):
+    check.dir(path)
+    if os.path.exists(os.path.join(location, extension, self)):
+        print('Already exists ' + self + extension + 'in' + os.path.expanduser(location))
+    else:
+        shutil.make_archive(self, extension, path)
