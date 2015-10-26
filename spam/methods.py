e4771b1be3d96722daaa100de096ed7eccda340a
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-10-26 12:52:53 +0100

    some slight change to create_dict function

--- spam/methods.py
+++ spam/methods.py
@@ -8,9 +8,11 @@ def insert_data(string, rep_dict):
 
     return pattern.sub(lambda x: rep_dict[x.group(0)], string)
 
-def create_dict(rep_dict, rep_list):
+def create_dict(rep_list):
 
-    for index, item in rep_list:
+    rep_dict = {}
+
+    for index, item in enumerate(rep_list):
         i = "{" + str(index) + "}"
         rep_dict[i] = item
 
