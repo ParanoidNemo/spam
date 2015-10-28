e7523ac248149e495ee33f7827b3390ae2032fde
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-10-28 11:06:13 +0100

    update docstring

--- spam/archive.py
+++ spam/archive.py
@@ -17,11 +17,26 @@
 #   Free Software Foundation, Inc.,
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+"""This module define a set of functions to check and compress and decompress
+archive of any types.
+
+Global functions defined:
+    - archive.compress: create an archive in a choose location
+    - archive.extract: exstract the files contained into a choose archive
+
+For more in depth usage and example check the docstring contained in the functions"""
+
 #import std module(s)
 import os, sys
 import shutil
 
 def compress(_path, location, name='new_archive', extension='tar'):
+
+    """This function create an archive in the specified location.
+    The default name is set to 'new_archive' and the default extension is '.tar'.
+    In order to use this function you have to specify the path to the files
+    you want to compress and the location where you want to create the archive"""
+
     while not os.path.isdir(os.path.expanduser(location)):
         os.makedirs(os.path.expanduser(location))
     else:
@@ -33,6 +48,10 @@ def compress(_path, location, name='new_archive', extension='tar'):
             shutil.make_archive(name, extension, base_dir=os.path.expanduser(_path))
 
 def extract(dst, location, name, extension='tar'):
+
+    """This function extract the files contained into a specified archive.
+    You have to specify the path of your archive, the name, the location where
+    you want to extract them and the extension of the archive (default: '.tar')"""
 
     try:
         archive_name = os.path.join(os.path.expanduser(location), name + '.' + extension)
