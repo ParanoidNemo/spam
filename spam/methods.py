b81650dfa38a4fea38679165819f31abf297fff3
Andrea Calzavacca <paranoid.nemo@gmail.com>
2015-10-28 13:18:13 +0100

    updated docstrings

--- spam/methods.py
+++ spam/methods.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
+"""This module define a set of functions to avoid repetitive operations.
+
+Global functions defined:
+    - insert_data: generate a string from a format string an a dict
+    - create_dict: generate a dictionary from a list
+    - format_string: generate a format string from an external file"""
+
 import os, re
 
 def insert_data(string, rep_dict):
+
+    """(string) return the string resulting by add the values contained in rep_dict
+    into another string that contains the dict keys"""
 
     pattern = re.compile("|".join([re.escape(k) for k in rep_dict.keys()]), re.M)
 
     return pattern.sub(lambda x: rep_dict[x.group(0)], string)
 
 def create_dict(rep_list):
+
+    """(dict) return a dict from a given list. The dict keys are progressive int
+    envelop by curled brachets"""
 
     rep_dict = {}
 
@@ -19,6 +32,8 @@ def create_dict(rep_list):
     return(rep_dict)
 
 def format_string(format_file):
+
+    """(string) return a html format string from a given file"""
 
     format_string = ''
 
